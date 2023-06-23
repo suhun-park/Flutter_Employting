93ee99c934b56ff9de91bfb12eb7d8e48e7ddd3d
hugesilver <xor3160@naver.com>
2023-06-23 22:44:12 +0900

    taeeun_v3.5.2

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
+import 'package:flutting/screen/signin/signin_screen.dart';
+import 'package:flutting/screen/signup/signup_screen.dart';
 import 'package:get/route_manager.dart';
 
 import '../root_tab/view/root_tab_view.dart';
-import 'home/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -13,11 +12,30 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("스플래시 이미지"),
+        title: const Text("스플래시 이미지"),
       ),
-      body: TextButton(
-        onPressed: () {Get.to(() => RootTab());
-      }, child: Text("넘기기"),),
+      body: Column(
+        children: [
+          TextButton(
+            onPressed: () {
+              Get.to(() => const RootTab());
+            },
+            child: const Text("넘기기"),
+          ),
+          TextButton(
+            onPressed: () {
+              Get.to(() => SignUpScreen());
+            },
+            child: const Text('회원가입'),
+          ),
+          TextButton(
+            onPressed: () {
+              Get.to(() => const SigninScreen());
+            },
+            child: const Text('로그인'),
+          ),
+        ],
+      ),
     );
   }
 }
