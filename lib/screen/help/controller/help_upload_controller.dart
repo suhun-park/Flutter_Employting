a918df0095aa692b5ecabe6fb9d4a846e9977a59
suhunhope <suhunhope@naver.com>
2023-06-24 02:23:56 +0900

    suhun_2:23

--- lib/screen/help/controller/help_upload_controller.dart
+++ lib/screen/help/controller/help_upload_controller.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'help_controller.dart';
 
@@ -7,6 +12,9 @@ class HelpUploadController extends GetxController{
   RxBool helpIsLoading = false.obs;
   RxString helpTitleText = "".obs;
   RxString helpContentText = "".obs;
+  RxString helpPickText = "".obs;
+  File? file;
+  Uint8List? fileBytes;
 
   void homeChangeTitle(value) {
     helpTitleText.value = value;
@@ -14,10 +22,28 @@ class HelpUploadController extends GetxController{
   void homeChangeContent(value) {
     helpContentText.value = value;
   }
+  Future<void> uploadPDF() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+
+    if (result != null) {
+      file = File(result.files.single.path!);
+      helpPickText.value = result.files.single.name;
+      fileBytes = result.files.first.bytes;
+    } else {
+      Get.back();
+    }
+  }
   void uploadPost(String id, DateTime dateTime) async {
     print('upload start');
     helpIsLoading = true.obs;
     try {
+      Reference storageRef = FirebaseStorage.instance.ref().child('help/${dateTime}_uid');
+      UploadTask uploadTask = storageRef.putData(fileBytes!);
+      TaskSnapshot taskSnapshot = await uploadTask;
+      String downloadURL = await taskSnapshot.ref.getDownloadURL();
       await FirebaseFirestore.instance.collection('help').doc(id).set({
         'uid': 'testUID',
         'nickName' :'asdf',
@@ -27,9 +53,11 @@ class HelpUploadController extends GetxController{
         'content' : helpContentText.value,
         'dept' : "컴퓨터정보학부"
       });
+      await FirebaseStorage.instance.ref('help/${dateTime}_uid').putData(fileBytes!);
       helpIsLoading = false.obs;
       helpTitleText = "".obs;
       helpContentText = "".obs;
+
 
       Get.back();
     } catch (e) {
