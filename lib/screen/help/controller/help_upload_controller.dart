3309c21c07f4f7dcb8a511699c88e99bc8ccfebf
suhunhope <suhunhope@naver.com>
2023-06-23 16:57:28 +0900

    suhun_v2

---
+++ lib/screen/help/controller/help_upload_controller.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'help_controller.dart';
+
+class HelpUploadController extends GetxController{
+  static HelpUploadController get to => Get.find();
+  RxBool helpIsLoading = false.obs;
+  RxString helpTitleText = "".obs;
+  RxString helpContentText = "".obs;
+
+  void homeChangeTitle(value) {
+    helpTitleText.value = value;
+  }
+  void homeChangeContent(value) {
+    helpContentText.value = value;
+  }
+  void uploadPost(String id, DateTime dateTime) async {
+    print('upload start');
+    helpIsLoading = true.obs;
+    try {
+      await FirebaseFirestore.instance.collection('help').doc(id).set({
+        'uid': 'testUID',
+        'nickName' :'asdf',
+        'pdf' : 'dsa',
+        'title': helpTitleText.value,
+        'dateTime': dateTime,
+        'content' : helpContentText.value,
+      });
+      helpIsLoading = false.obs;
+      helpTitleText = "".obs;
+      helpContentText = "".obs;
+
+      Get.back();
+    } catch (e) {
+      print(e.toString());
+      helpIsLoading = false.obs;
+    }
+  }
+
+}
